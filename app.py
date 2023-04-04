d1d901989231f4303a8e06a61bd67430f9cc7f5c
Zaki Aghil <dzakiaha@amazon.com>
2023-04-04 15:24:32 +0100

    modified app file

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import Flask,render_template,request,redirect
-from models import db,FighterModel
+from models import db, FighterModel
+from flask import abort
+
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///<FightersDB>.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -23,9 +26,60 @@ def create():
         name = request.form['name']
         video_game = request.form['video_game']
         console = request.form['console']
-        fighter = FighterModel(Figher_id=Fighter_id, name=name, video_game=video_game, console=console)
-        db.session.add(fighter)
+        fighters = FighterModel(Figher_id=Fighter_id, name=name, video_game=video_game, console=console)
+        db.session.add(fighters)
         db.session.commit()
         return redirect('/data')
+    
+    """retrieve code"""
+   
+   
+@app.route('/data')
+def RetrieveDataList():
+    fighters = FighterModel.query.all()
+    return render_template('datalist.html', fighters = fighters)
+
+@app.route('/data/<int:id>')
+def RetrieveSingleEmployee(id):
+    fighter = FighterModel.query.filter_by(Fighter_id=id).first()
+    if fighter:
+        return render_template('data.html', fighter = fighter)
+    return f"Figher with id ={id} Doenst exist"
+
+"Update Code"
+
+@app.route('/data/<int:id>/update',methods = ['GET','POST'])
+def update(id):
+    employee = FighterModel.query.filter_by(Fighter_id=id).first()
+    if request.method == 'POST':
+        if fighter:
+            db.session.delete(fighter)
+            db.session.commit()
+ 
+            name = request.form['name']
+            video_game = request.form['video_game']
+            console = request.form['console']
+            fighter = FighterModel(Fighter_id=id, name=name, video_game=video_game, console= console)
+ 
+            db.session.add(fighter)
+            db.session.commit()
+            return redirect(f'/data/{id}')
+        return f"Fighter with id = {id} Does nit exist"
+ 
+    return render_template('update.html', fighter = fighter)
+
+"delete code"
+
+@app.route('/data/<int:id>/delete', methods=['GET','POST'])
+def delete(id):
+    fighter = FighterModel.query.filter_by(Fighter_id=id).first()
+    if request.method == 'POST':
+        if fighter:
+            db.session.delete(fighter)
+            db.session.commit()
+            return redirect('/data')
+        abort(404)
+ 
+    return render_template('delete.html')
  
 app.run(host='localhost', port=5000)
