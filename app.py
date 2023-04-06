5cf6d34dfa2ff0edb1e3522aa03b6c55cd52be6c
Zaki Aghil <dzakiaha@amazon.com>
2023-04-06 10:12:29 +0100

    update files

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
 from flask import Flask,render_template,request,redirect
-from models import db, FighterModel
+from models import db,FighterModel
 from flask import abort
 
-
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///<FightersDB>.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -22,11 +21,11 @@ def create():
         return render_template('createpage.html')
  
     if request.method == 'POST':
-        Fighter_id = request.form['Fighter_id']
+        fighter_id = request.form['fighter_id']
         name = request.form['name']
         video_game = request.form['video_game']
         console = request.form['console']
-        fighters = FighterModel(Figher_id=Fighter_id, name=name, video_game=video_game, console=console)
+        fighters = FighterModel(fighter_id=fighter_id, name=name, video_game=video_game, console=console)
         db.session.add(fighters)
         db.session.commit()
         return redirect('/data')
@@ -41,16 +40,16 @@ def RetrieveDataList():
 
 @app.route('/data/<int:id>')
 def RetrieveSingleEmployee(id):
-    fighter = FighterModel.query.filter_by(Fighter_id=id).first()
+    fighter = FighterModel.query.filter_by(fighter_id=id).first()
     if fighter:
         return render_template('data.html', fighter = fighter)
-    return f"Figher with id ={id} Doenst exist"
+    return f"fighter with id ={id} Doenst exist"
 
 "Update Code"
 
 @app.route('/data/<int:id>/update',methods = ['GET','POST'])
 def update(id):
-    employee = FighterModel.query.filter_by(Fighter_id=id).first()
+    fighter = FighterModel.query.filter_by(fighter_id=id).first()
     if request.method == 'POST':
         if fighter:
             db.session.delete(fighter)
@@ -59,20 +58,19 @@ def update(id):
             name = request.form['name']
             video_game = request.form['video_game']
             console = request.form['console']
-            fighter = FighterModel(Fighter_id=id, name=name, video_game=video_game, console= console)
- 
+            fighter = FighterModel(fighter_id=id, name=name, video_game=video_game, console= console)
             db.session.add(fighter)
             db.session.commit()
             return redirect(f'/data/{id}')
-        return f"Fighter with id = {id} Does nit exist"
+        return f"fighter with id = {id} Does nit exist"
  
-    return render_template('update.html', fighter = fighter)
+    return render_template('update.html',fighter = fighter)
 
 "delete code"
 
 @app.route('/data/<int:id>/delete', methods=['GET','POST'])
 def delete(id):
-    fighter = FighterModel.query.filter_by(Fighter_id=id).first()
+    fighter = FighterModel.query.filter_by(fighter_id=id).first()
     if request.method == 'POST':
         if fighter:
             db.session.delete(fighter)
@@ -81,5 +79,5 @@ def delete(id):
         abort(404)
  
     return render_template('delete.html')
- 
+app.debug = True 
 app.run(host='localhost', port=5000)
--- models.py
+++ models.py
@@ -1,21 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
- 
-db = SQLAlchemy()
+
+db =SQLAlchemy()
  
 class FighterModel(db.Model):
-    __tablename__ = 'Fighters'
+    __tablename__ = 'Fighter'
  
     id = db.Column(db.Integer, primary_key = True)
-    Fighter_id = db.Column(db.Integer(),unique = True)
-    name = db.Column(db.string())
-    video_game = db.Column(db.string())
-    console = db.coluum(db.string())
+    fighter_id = db.Column(db.Integer(),unique = True)
+    name = db.Column(db.String(15))
+    video_game = db.Column(db.String(20))
+    console = db.Column(db.String(10))
+    
  
-    def __init__(self,Fighter_id,name,video_game,console):
-        self.Fighter_id = Fighter_id
+    def __init__(self,fighter_id,name,video_game,console):
+        self.fighter_id = fighter_id
         self.name = name
         self.video_game = video_game
         self.console = console
  
     def __repr__(self):
-        return f"{self.Fighter_id}:{self.name}:{self.video_game}{self.console}"
+        return f"{self.fighter_id}:{self.name}"
